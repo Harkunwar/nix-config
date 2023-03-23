6ad2cfdfbc23553005e5bf0775939e59f2e6a731
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2023-03-23 08:41:15 -0700

    helix theme change and config cleanup

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,12 +1,18 @@
 ({ pkgs, ... }: {
   # Specify my home-manager configs
   # Guide: https://nix-community.github.io/home-manager/options.html
-  home.stateVersion = "22.11"; # Don't change this, leave it alone
-  home.packages = with pkgs; [ ripgrep fd curl less ];
-  home.sessionVariables = {
-    PAGER = "less";
-    CLICOLOR = 1;
-    EDITOR = "hx";
+  home = {
+    stateVersion = "22.11"; # Don't change this, leave it alone
+    packages = with pkgs; [ ripgrep fd curl less ];
+    sessionVariables = {
+      PAGER = "less";
+      CLICOLOR = 1;
+      EDITOR = "hx";
+    };
+    file = {
+      ".inputrc".source = ./dotfiles/inputrc;
+      ".config/helix/config.toml".source = ./dotfiles/config/helix/config.toml;
+    };
   };
   programs.bat.enable = true;
   programs.bat.config.theme = "TwoDark";
@@ -24,19 +30,21 @@
       cm = "commit -m";
     };
   };
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.enableSyntaxHighlighting = true;
-  programs.zsh.prezto.tmux.itermIntegration = true;
-  programs.zsh.shellAliases = { 
-    ls = "ls --color=auto -F";
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    prezto.tmux.itermIntegration = true;
+    shellAliases = { 
+      ls = "ls --color=auto -F";
 
-    nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
-    nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd;";  
+      cat = "bat";
+
+      nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
+      nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd;";  
+    };
   };
   programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
-  home.file.".inputrc".source = ./dotfiles/inputrc;
-  home.file.".config/helix/config.toml".source = ./dotfiles/config/helix/config.toml;
 })
