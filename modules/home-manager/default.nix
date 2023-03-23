74a1ecb396d4514c7e912445f9e12af2c8c3decf
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2023-03-23 08:47:54 -0700

    More config cleanup

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -14,37 +14,45 @@
       ".config/helix/config.toml".source = ./dotfiles/config/helix/config.toml;
     };
   };
-  programs.bat.enable = true;
-  programs.bat.config.theme = "TwoDark";
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
-  programs.exa.enable = true;
-  programs.git = {
-    enable = true;
-    userName = "Harkunwar Kochar";
-    userEmail = "10580591+Harkunwar@users.noreply.github.com";
-    delta.enable = true;
-    aliases = {
-      ph = "push origin head";
-      au = "add -u";
-      cm = "commit -m";
+  programs = {
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
+    };
+    fzf = { 
+      enable = true;
+      enableZshIntegration = true;
+    };
+    exa.enable = true;
+    git = {
+      enable = true;
+      userName = "Harkunwar Kochar";
+      userEmail = "10580591+Harkunwar@users.noreply.github.com";
+      delta.enable = true;
+      aliases = {
+        ph = "push origin head";
+        au = "add -u";
+        cm = "commit -m";
+      };
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      prezto.tmux.itermIntegration = true;
+      shellAliases = { 
+        ls = "ls --color=auto -F";
+
+        cat = "bat";
+
+        nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
+        nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd;";  
+      };
+    };
+    starship = { 
+      enable = true;
+      enableZshIntegration = true;
     };
   };
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    prezto.tmux.itermIntegration = true;
-    shellAliases = { 
-      ls = "ls --color=auto -F";
-
-      cat = "bat";
-
-      nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
-      nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd;";  
-    };
-  };
-  programs.starship.enable = true;
-  programs.starship.enableZshIntegration = true;
 })
