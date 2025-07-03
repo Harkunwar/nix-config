ac34945895d386c23cf07b4dda32d0874ec68acb
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-03 07:10:52 +0000

    small fixes

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nixosConfigurations.node804 = nixpkgs.lib.nixosSystem rec {
       pkgs = import nixpkgs { inherit system; };
       system = "x86_64-linux";
-      modules = [ ./configuration.nix
+      modules = [ ./modules/nixos/configuration.nix
                   # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
                   ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
                 ];
