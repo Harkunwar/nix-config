06b07f90f46523b5648cfda3d3f7666ad692db40
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-03 01:22:34 -0700

    extract out hosts

---
+++ hosts/macbook-pro-14/default.nix
@@ -0,0 +1,19 @@
+({ inputs, nixpkgs, home-manager, darwin, ... }:
+
+  darwin.lib.darwinSystem {
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs { system = "aarch64-darwin"; config.allowUnfree = true; };
+    modules = [
+      ../../modules/darwin
+      home-manager.darwinModules.home-manager
+      {
+        users.users.harkunwar.home = "/Users/harkunwar";
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.harkunwar.imports = [ ../../modules/home-manager ];
+        };
+      }
+    ];
+  }
+)
---
+++ hosts/node804/default.nix
@@ -0,0 +1,11 @@
+({ inputs, nixpkgs, home-manager ... }:
+    nixpkgs.lib.nixosSystem rec {
+        pkgs = import nixpkgs { inherit system; };
+        system = "x86_64-linux";
+        modules = [
+            ../../modules/nixos/configuration.nix
+            # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
+            ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
+        ];
+    }
+)
