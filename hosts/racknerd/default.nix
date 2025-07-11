ba1fdf25ae6b0ae85fb3f6424a144df82079616e
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 14:05:56 -0700

    try this

--- hosts/racknerd/default.nix
+++ hosts/racknerd/default.nix
@@ -3,6 +3,7 @@ nixpkgs.lib.nixosSystem rec {
   pkgs = import nixpkgs { inherit system; };
   system = "x86_64-linux";
   modules = [
+    inputs.sops-nix.nixosModules.sops
     ../common/core/sops.nix
     ./configuration.nix
     # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
