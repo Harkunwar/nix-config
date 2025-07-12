8a9f32c8838eff952dec9b06893c6ef179ff4f4d
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 21:16:42 -0700

    add pkg

--- hosts/node804/services/pocketid.nix
+++ hosts/node804/services/pocketid.nix
@@ -1,9 +1,4 @@
 { config, pkgs, ... }:
-let
-  pocketidPort = 1441;
-  pocketidDataDir = "/var/lib/pocketid";
-  pocketidPackage = pkgs.unstable.pocket-id;
-in
 {
     sops = {
         secrets = {
@@ -12,58 +7,22 @@ in
         templates = {
             "pocketid-env".content = ''
                 MAXMIND_LICENSE_KEY=${config.sops.placeholder."MAXMIND_LICENSE_KEY"}
-                TRUST_PROXY=true
-                PUBLIC_APP_URL=http://pocketid.lab.harkunwar.com
-                PUBLIC_UI_CONFIG_DISABLED=false
-                PORT=${toString pocketidPort}
-                DATA_DIR=${pocketidDataDir}
             '';
         };
     };
 
-    # Create pocketid user
-    users.users.pocketid = {
-        isSystemUser = true;
-        group = "pocketid";
-        home = pocketidDataDir;
-        createHome = true;
+    # Override the pocket-id package to use the unstable version
+    nixpkgs.config.packageOverrides = pkgs: {
+        pocket-id = pkgs.unstable.pocket-id;
     };
-    users.groups.pocketid = {};
 
-    # Create data directory with proper permissions
-    systemd.tmpfiles.rules = [
-        "d ${pocketidDataDir} 0755 pocketid pocketid -"
-    ];
-
-    # PocketID systemd service using nixpkgs package
-    systemd.services.pocketid = {
-        description = "PocketID Service";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        
-        serviceConfig = {
-            Type = "simple";
-            User = "pocketid";
-            Group = "pocketid";
-            WorkingDirectory = pocketidDataDir;
-            EnvironmentFile = config.sops.templates."pocketid-env".path;
-            ExecStart = "${pocketidPackage}/bin/pocket-id";
-            Restart = "always";
-            RestartSec = "10";
-            
-            # Security hardening
-            NoNewPrivileges = true;
-            ProtectSystem = "strict";
-            ProtectHome = true;
-            ReadWritePaths = [ pocketidDataDir ];
-            PrivateTmp = true;
-            ProtectKernelTunables = true;
-            ProtectKernelModules = true;
-            ProtectControlGroups = true;
+    services.pocket-id = {
+        enable = true;
+        port = 1441;
+        settings = {
+            TRUST_PROXY = true;
+            APP_URL = "http://pocketid.lab.harkunwar.com";
         };
-        
-        environment = {
-            NODE_ENV = "production";
-        };
+        environmentFile = config.sops.templates."pocketid-env".path;
     };
 }
