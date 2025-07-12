35f7d9208db87b3d003cad7543b676c441d12bf9
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 20:55:51 -0700

    add pkg

--- hosts/node804/services/caddy.nix
+++ hosts/node804/services/caddy.nix
@@ -48,7 +48,7 @@
             dns cloudflare {env.CLOUDFLARE_EDIT_ALL_ZONE_API_KEY}
           }
           
-          reverse_proxy locahost:1411
+          reverse_proxy locahost:3000
         '';
       };
       
