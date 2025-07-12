45aaf661e7baa43b7759077de928e029b0e4bf0a
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 21:02:32 -0700

    add pkg

--- hosts/node804/services/caddy.nix
+++ hosts/node804/services/caddy.nix
@@ -42,7 +42,7 @@
       };
 
       # Pocket ID service
-      "pocketid.lab.harkunwar.com" = {
+      "pocketid.lab.harkunwar.com:3000" = {
         extraConfig = ''
           tls {
             dns cloudflare {env.CLOUDFLARE_EDIT_ALL_ZONE_API_KEY}
