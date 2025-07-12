1d37298295f0da3824ca0fc3738f5556878cb7c2
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 20:41:44 -0700

    add pkg

--- hosts/node804/services/pocketid.nix
+++ hosts/node804/services/pocketid.nix
@@ -13,11 +13,10 @@
 
     services.pocket-id = {
         enable = true;
-        package = pkgs.unstable.pocket-id;
+        package = pkgs.pocket-id;
         settings = {
             TRUST_PROXY = true;
             APP_URL = "http://pocketid.lab.harkunwar.com";
-            PUBLIC_APP_URL = "https://pocketid.lab.harkunwar.com";
             PORT = 1411;
         };
         environmentFile = "${config.sops.templates."pocketid-env".path}";
