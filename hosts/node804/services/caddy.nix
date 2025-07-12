a82dc6f25285e45cfceb0c5774b475fbaf410d5d
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 17:47:04 -0700

    try to add caddy dns support

--- hosts/node804/services/caddy.nix
+++ hosts/node804/services/caddy.nix
@@ -3,6 +3,11 @@
   services.caddy = {
     enable = true;
 
+    ackage = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@35fb8474f57d7476329f75d63eebafb95a93022f" ];
+      hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+    };
+
     virtualHosts = {
       # "immich.lab.harkunwar.com" = {
       #   extraConfig = ''
