15b6c1d88981d2726e340a3723bbdcefd046bb4a
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 21:30:04 -0700

    add pkg

--- hosts/node804/services/pocketid.nix
+++ hosts/node804/services/pocketid.nix
@@ -11,15 +11,15 @@
         };
     };
 
-    services.pocket-id = {
-        enable = true;
-        package = pkgs.unstable.pocket-id;
-        settings = {
-            APP_URL = "http://pocketid.lab.harkunwar.com";
-            TRUST_PROXY = true;
-            PORT = 1441;
-        };
-        environmentFile = config.sops.templates."pocketid-env".path;
-    };
+    # services.pocket-id = {
+    #     enable = true;
+    #     package = pkgs.unstable.pocket-id;
+    #     settings = {
+    #         APP_URL = "http://pocketid.lab.harkunwar.com";
+    #         TRUST_PROXY = true;
+    #         PORT = 1441;
+    #     };
+    #     environmentFile = config.sops.templates."pocketid-env".path;
+    # };
 
 }
