7a7452d9f527a95970a35e362fdfd05bf378445c
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 19:39:03 -0700

    fix env var

--- hosts/node804/services/pocketid.nix
+++ hosts/node804/services/pocketid.nix
@@ -12,7 +12,7 @@
     };
 
     services.pocket-id = {
-        enable = true;
+        enable = false;
         settings = {
             TRUST_PROXY = true;
             PUBLIC_APP_URL = "https://pocketid.lab.harkunwar.com";
