c96482b1e18ddad0986ba5372c8d8d23e1700131
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 21:22:18 -0700

    add pkg

--- hosts/node804/services/pocketid.nix
+++ hosts/node804/services/pocketid.nix
@@ -7,6 +7,7 @@
         templates = {
             "pocketid-env".content = ''
                 MAXMIND_LICENSE_KEY=${config.sops.placeholder."MAXMIND_LICENSE_KEY"}
+                PORT=1441
             '';
         };
     };
@@ -15,10 +16,12 @@
         enable = true;
         package = pkgs.unstable.pocket-id;
         settings = {
-            PORT = 1441;
-            TRUST_PROXY = true;
             APP_URL = "http://pocketid.lab.harkunwar.com";
+            TRUST_PROXY = true;
         };
         environmentFile = config.sops.templates."pocketid-env".path;
     };
+
+    # Open firewall for the specified port
+    networking.firewall.allowedTCPPorts = [ 1441 ];
 }
