0786fd357c408669d368963b362d32138804d517
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-12 00:11:13 -0700

    add rathole client

--- hosts/node804/default.nix
+++ hosts/node804/default.nix
@@ -9,6 +9,7 @@ nixpkgs.lib.nixosSystem rec {
     ./configuration.nix
     ./services/pocketid.nix
     ./services/caddy.nix
+    ./services/rathole-client.nix
     # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
     ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
     # Make unstable packages available
