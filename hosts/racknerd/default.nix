02d431a4e11bb2c448940d98a724cd7a9d0b162c
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-12 00:33:44 -0700

    remove rathole

--- hosts/racknerd/default.nix
+++ hosts/racknerd/default.nix
@@ -6,7 +6,6 @@ nixpkgs.lib.nixosSystem rec {
     inputs.sops-nix.nixosModules.sops
     ../common/core/sops.nix
     ./configuration.nix
-    ./services/rathole-server.nix
     ./services/wireguard-server.nix
     # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
     ({ config, pkgs, options, ... }: { nix.registry.nixpkgs.flake = nixpkgs; })
