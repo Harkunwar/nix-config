8e0d8aa63072f023aeb41bb6f8e9734b2b71758b
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-12 11:19:12 -0700

    wg

--- hosts/racknerd/services/wireguard-server.nix
+++ hosts/racknerd/services/wireguard-server.nix
@@ -116,11 +116,6 @@ in
           proto = "tcp";
           sourcePort = 443;
         }
-        {
-          destination = "10.100.0.101:4664";
-          proto = "tcp";
-          sourcePort = 4664;
-        }
       ];
     };
   };
