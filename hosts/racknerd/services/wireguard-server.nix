fb361d9e4f7cf28a51fed6ee9754d21cea41f7ae
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-12 01:28:57 -0700

    try portforwarding

--- hosts/racknerd/services/wireguard-server.nix
+++ hosts/racknerd/services/wireguard-server.nix
@@ -118,12 +118,28 @@ in
 
       # Add missing postSetup
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens3 -j MASQUERADE
+        
+        # Port forwarding rules - forward HTTP/HTTPS to node804
+        ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -i ens3 -p tcp --dport 80 -j DNAT --to-destination 10.100.0.101:80
+        ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -i ens3 -p tcp --dport 443 -j DNAT --to-destination 10.100.0.101:443
+        
+        # Allow forwarding for these ports
+        ${pkgs.iptables}/bin/iptables -A FORWARD -p tcp -d 10.100.0.101 --dport 80 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -A FORWARD -p tcp -d 10.100.0.101 --dport 443 -j ACCEPT
       '';
 
       # This undoes the above command
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ens3 -j MASQUERADE
+        
+        # Remove port forwarding rules
+        ${pkgs.iptables}/bin/iptables -t nat -D PREROUTING -i ens3 -p tcp --dport 80 -j DNAT --to-destination 10.100.0.101:80
+        ${pkgs.iptables}/bin/iptables -t nat -D PREROUTING -i ens3 -p tcp --dport 443 -j DNAT --to-destination 10.100.0.101:443
+        
+        # Remove forward rules
+        ${pkgs.iptables}/bin/iptables -D FORWARD -p tcp -d 10.100.0.101 --dport 80 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -D FORWARD -p tcp -d 10.100.0.101 --dport 443 -j ACCEPT
       '';
 
       # Path to the private key file managed by sops
@@ -138,6 +154,7 @@ in
   networking.firewall = {
     enable = true;
     allowedUDPPorts = [ 61899 ];
+    allowedTCPPorts = [ 80 443 ];
   };
 
 }
