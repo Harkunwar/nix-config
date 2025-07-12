ed585a0a19afc306df96dd1ad0e19b06b8988042
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2025-07-11 20:16:48 -0700

    add pkg

--- hosts/node804/default.nix
+++ hosts/node804/default.nix
@@ -3,6 +3,7 @@ nixpkgs.lib.nixosSystem rec {
   pkgs = import nixpkgs { inherit system; };
   pkgs-unstable = import nixpkgs-unstable { inherit system; };
   system = "x86_64-linux";
+  specialArgs = { inherit pkgs-unstable; };
   modules = [
     inputs.sops-nix.nixosModules.sops
     ../common/core/sops.nix
